5325949f789e00c52725e0db64b1ded65937eb37
BTWitsK <BTWitsK@Gmail.com>
2021-12-26 12:37:23 -0600

    working on stage 2

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,15 +1,54 @@
 package bullscows;
 
+import java.util.*;
+
+class Game{
+    static int bulls = 0;
+    static int cows = 0;
+    static ArrayList<String> code = new ArrayList<>();
+
+
+    public void Grader(List guess) {
+
+
+
+
+
+
+    }
+
+    public void printResults() {
+        if (bulls == 0 && cows == 0) {
+            System.out.println("Grade: None. The secret code is 2474");
+        } else if (bulls == 0) {
+            System.out.printf("Grade: %d cow(s). The secret code is 2474 \n", cows);
+        } else if (cows == 0) {
+            System.out.printf("Grade: %d bull(s). The secret code is 2474 \n", bulls);
+        } else {
+            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is 2474 \n", bulls, cows);
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The secret code is prepared: ****");
-        System.out.println("Turn 1. Answer:");
-        System.out.println("1234");
-        System.out.println("Grade: None.");
-        System.out.println("");
-        System.out.println("Turn 2. Answer:");
-        System.out.println("9876");
-        System.out.println("Grade: 4 bulls.");
-        System.out.println("Congrats! The secret code is 9876.");
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> secret = new ArrayList<>();
+
+
+        List<Integer> input = new ArrayList<>();
+
+        for (String num : String.valueOf(scanner.nextInt()).split("")) {
+            input.add(Integer.valueOf(num));
+        }
+
+
+
+
+
+
     }
+
+
+
 }
